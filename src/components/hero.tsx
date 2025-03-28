656b181dd066ded374f5a4547d6b7195800c66d2
hendriam <hendriam18@gmail.com>
2025-03-28 16:25:56 +0700

    remove framer-motion from hero component and simplify animation handling

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,35 +1,10 @@
 import imgHero from "../assets/img-hero.jpg";
 import vector1 from "../assets/vector-1.svg";
-import { motion } from "motion/react";
-
-const fadeInLeftVariants = {
-    initial: { x: -100 },
-    animate: {
-        x: 0,
-        transition: { duration: 1.5, ease: "easeInOut" },
-    },
-};
-
-const fadeInRightVariants = {
-    initial: { x: 100 },
-    animate: {
-        x: 0,
-        transition: { duration: 1.5, ease: "easeInOut" },
-    },
-};
 
 export default function Hero() {
     return (
         <div className="container flex justify-between items-center pt-5 pb-20 space-x-5 mx-0 lg:mx-auto px-5 xl:px-0 lg:w-full max-w-6xl">
-            <motion.div
-                className="flex flex-col items-start lg:space-y-11 space-y-5"
-                variants={fadeInLeftVariants}
-                initial="initial"
-                whileInView="animate"
-                viewport={{
-                    once: true,
-                }}
-            >
+            <div className="flex flex-col items-start lg:space-y-11 space-y-5">
                 <h1 className="text-white lg:text-7xl md:text-6xl sm:text-5xl text-5xl lg:leading-23 md:leading-20 sm:leading-17 leading-16 transition-all delay-75 duration-300 ease-in-out">
                     Your Partner on the{" "}
                     <span>
@@ -51,17 +26,11 @@ export default function Hero() {
                 <button className="bg-[#FCA311] hover:opacity-80 hover:cursor-pointer md:py-3 py-2 md:px-5 px-3.5 xl:text-2xl md:text-xl text-lg text-center font-bold transition-opacity delay-100 duration-300 ease-in-out">
                     Schedule a Consultation
                 </button>
-            </motion.div>
-            <motion.img
-                variants={fadeInRightVariants}
-                initial="initial"
-                whileInView="animate"
-                viewport={{
-                    once: true,
-                }}
+            </div>
+            <img
                 src={imgHero}
                 alt="img-hero"
-                className="hidden md:flex lg:w-[400px] md:w-[300px] w-[250px]"
+                className="hidden md:flex lg:w-[400px] md:w-[300px] w-[250px] transition-all delay-75 duration-300 ease-in-out rotate-0 hover:rotate-10"
             />
         </div>
     );
